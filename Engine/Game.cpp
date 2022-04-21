60112add17552662d1451703f76d0d3e351c9f99
Farokh <farokhbahalou@yahoo.com>
2022-04-20 22:09:12 -0400

    doing per pixel lighting (phong shading without specular)

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -32,11 +32,14 @@
 #include "GeometryFlatScene.h"
 #include "GouraudScene.h"
 #include "GouraudPointScene.h"
+#include "PhongPointScene.h"
 #include <sstream>
 
 Game::Game( MainWindow& wnd ) : wnd( wnd ), gfx(wnd)
 {
-	scenes.push_back(std::make_unique<GouraudPointScene>(gfx, Plane::GetNormals<GouraudPointScene::Vertex>(1)));
+	scenes.push_back(std::make_unique<PhongPointScene>(gfx, IndexedTriangleList<PhongPointScene::Vertex>::LoadNormals("models\\suzanne.obj")));
+	scenes.push_back(std::make_unique<PhongPointScene>(gfx, Plane::GetNormals<PhongPointScene::Vertex>(16)));
+	scenes.push_back(std::make_unique<GouraudPointScene>(gfx, Plane::GetNormals<GouraudPointScene::Vertex>(16)));
 	scenes.push_back(std::make_unique<GouraudPointScene>(gfx, IndexedTriangleList<GouraudPointScene::Vertex>::LoadNormals("models\\suzanne.obj")));
 	scenes.push_back(std::make_unique<GouraudScene>(gfx, IndexedTriangleList<GouraudScene::Vertex>::LoadNormals("models\\suzanne.obj")));
 	scenes.push_back(std::make_unique<GouraudScene>(gfx, Sphere::GetPlainNormals<GouraudScene::Vertex>()));
