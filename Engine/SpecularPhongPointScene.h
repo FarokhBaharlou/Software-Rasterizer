d049cb47ed136bccaa42f7951ffa6ac7d0829f08
Farokh <farokhbahalou@yahoo.com>
2022-04-25 22:40:15 -0400

    added camera transformation to pipeline

--- Engine/SpecularPhongPointScene.h
+++ Engine/SpecularPhongPointScene.h
@@ -7,6 +7,7 @@
 #include "SpecularPhongPointEffect.h"
 #include "SolidEffect.h"
 #include "Sphere.h"
+#include "MouseTracker.h"
 
 class SpecularPhongPointScene : public Scene
 {
@@ -18,7 +19,7 @@ public:
 	SpecularPhongPointScene(Graphics& gfx, IndexedTriangleList<Vertex> tl) : itlist(std::move(tl)), pZb(std::make_shared<ZBuffer>(gfx.ScreenWidth, gfx.ScreenHeight)), pipeline(gfx, pZb), liPipeline(gfx, pZb), Scene("phong point shader scene free mesh")
 	{
 		itlist.AdjustToTrueCenter();
-		offset_z = itlist.GetRadius() * 1.6f;
+		mod_pos.z = itlist.GetRadius() * 1.6f;
 		for (auto& v : lightIndicator.vertices)
 		{
 			v.color = Colors::White;
@@ -26,89 +27,80 @@ public:
 	}
 	virtual void Update(Keyboard& kbd, Mouse& mouse, float dt) override
 	{
-		if (kbd.KeyIsPressed('Q'))
-		{
-			theta_x = wrap_angle(theta_x + dTheta * dt);
-		}
 		if (kbd.KeyIsPressed('W'))
 		{
-			theta_y = wrap_angle(theta_y + dTheta * dt);
-		}
-		if (kbd.KeyIsPressed('E'))
-		{
-			theta_z = wrap_angle(theta_z + dTheta * dt);
+			cam_pos += Vec4{ 0.0f,0.0f,1.0f,0.0f } * !cam_rot_inv * cam_speed * dt;
 		}
 		if (kbd.KeyIsPressed('A'))
 		{
-			theta_x = wrap_angle(theta_x - dTheta * dt);
+			cam_pos += Vec4{ -1.0f,0.0f,0.0f,0.0f } * !cam_rot_inv * cam_speed * dt;
 		}
 		if (kbd.KeyIsPressed('S'))
 		{
-			theta_y = wrap_angle(theta_y - dTheta * dt);
+			cam_pos += Vec4{ 0.0f,0.0f,-1.0f,0.0f } * !cam_rot_inv * cam_speed * dt;
 		}
 		if (kbd.KeyIsPressed('D'))
 		{
-			theta_z = wrap_angle(theta_z - dTheta * dt);
+			cam_pos += Vec4{ 1.0f,0.0f,0.0f,0.0f } * !cam_rot_inv * cam_speed * dt;
 		}
-		if (kbd.KeyIsPressed('U'))
+		if (kbd.KeyIsPressed('C'))
 		{
-			lpos_x += 0.2f * dt;
+			cam_pos += Vec4{ 0.0f,1.0f,0.0f,0.0f } *!cam_rot_inv * cam_speed * dt;
 		}
-		if (kbd.KeyIsPressed('I'))
+		if (kbd.KeyIsPressed('Z'))
 		{
-			lpos_y += 0.2f * dt;
+			cam_pos += Vec4{ 0.0f,-1.0f,0.0f,0.0f } *!cam_rot_inv * cam_speed * dt;
 		}
-		if (kbd.KeyIsPressed('O'))
+		if (kbd.KeyIsPressed('Q'))
 		{
-			lpos_z += 0.2f * dt;
+			cam_rot_inv = cam_rot_inv * Mat4::RotationZ(cam_roll_speed * dt);
 		}
-		if (kbd.KeyIsPressed('J'))
+		if (kbd.KeyIsPressed('E'))
 		{
-			lpos_x -= 0.2f * dt;
+			cam_rot_inv = cam_rot_inv * Mat4::RotationZ(-cam_roll_speed * dt);
 		}
-		if (kbd.KeyIsPressed('K'))
+
+		while (!mouse.IsEmpty())
 		{
-			lpos_y -= 0.2f * dt;
-		}
-		if (kbd.KeyIsPressed('L'))
-		{
-			lpos_z -= 0.2f * dt;
-		}
-		if (kbd.KeyIsPressed('R'))
-		{
-			offset_z += 0.7f * dt;
-		}
-		if (kbd.KeyIsPressed('F'))
-		{
-			offset_z -= 0.7f * dt;
-		}
-		if (kbd.KeyIsPressed('N'))
-		{
-			phi -= 1.8f * dt;
-		}
-		if (kbd.KeyIsPressed('M'))
-		{
-			phi += 1.8f * dt;
+			const auto e = mouse.Read();
+			switch (e.GetType())
+			{
+			case Mouse::Event::Type::LPress:
+				mt.Engage(e.GetPos());
+				break;
+			case Mouse::Event::Type::LRelease:
+				mt.Release();
+				break;
+			case Mouse::Event::Type::Move:
+				if (mt.Engaged())
+				{
+					const auto delta = mt.Move(e.GetPos());
+					cam_rot_inv = cam_rot_inv * Mat4::RotationY((float)-delta.x * htrack) * Mat4::RotationX((float)-delta.y * vtrack);
+				}
+				break;
+			}
 		}
 	}
 	virtual void Draw() override
 	{
 		pipeline.BeginFrame();
 
-		const auto proj = Mat4::ProjectionHFOV(100.0f, 1.33333f, 0.5f, 4.0f);
+		const auto proj = Mat4::ProjectionHFOV(hfov, aspect_ratio, 0.5f, 4.0f);
+		const auto view = Mat4::Translation(-cam_pos) * cam_rot_inv;
 		// generate rotation matrix from euler angles
 		// translation from offset
 		// set pipeline transform
-		pipeline.effect.vs.BindWorld(Mat4::RotationX(theta_x) * Mat4::RotationY(theta_y) * Mat4::RotationZ(theta_z) * Mat4::Translation(0.0f, 0.0f, offset_z) * Mat4::RotationY(phi));
+		pipeline.effect.vs.BindWorld(Mat4::RotationX(theta_x) * Mat4::RotationY(theta_y) * Mat4::RotationZ(theta_z) * Mat4::Translation(mod_pos));
+		pipeline.effect.vs.BindView(view);
 		pipeline.effect.vs.BindProjection(proj);
-		pipeline.effect.ps.SetLightPosition({ lpos_x,lpos_y,lpos_z });
+		pipeline.effect.ps.SetLightPosition(l_pos * view);
 		// render triangles
 		pipeline.Draw(itlist);
 
 		// draw light indicator with different pipeline
 		// don't call beginframe on this pipeline b/c wanna keep zbuffer contents
 		// (don't like this assymetry but we'll live with it for now)
-		liPipeline.effect.vs.BindWorld(Mat4::Translation(lpos_x, lpos_y, lpos_z));
+		liPipeline.effect.vs.BindWorldView(Mat4::Translation(l_pos) * view);
 		liPipeline.effect.vs.BindProjection(proj);
 		liPipeline.Draw(lightIndicator);
 	}
@@ -118,13 +110,23 @@ private:
 	std::shared_ptr<ZBuffer> pZb;
 	Pipeline pipeline;
 	LightIndicatorPipeline liPipeline;
-	static constexpr float dTheta = PI;
-	float offset_z = 2.0f;
+	MouseTracker mt;
+	// fov
+	static constexpr float aspect_ratio = 1.33333f;
+	static constexpr float hfov = 95.0f;
+	static constexpr float vfov = hfov / aspect_ratio;
+	// camera stuff
+	static constexpr float htrack = to_rad(hfov) / (float)Graphics::ScreenWidth;
+	static constexpr float vtrack = to_rad(vfov) / (float)Graphics::ScreenHeight;
+	static constexpr float cam_speed = 1.0f;
+	static constexpr float cam_roll_speed = PI;
+	Vec3 cam_pos = { 0.0f,0.0f,0.0f };
+	Mat4 cam_rot_inv = Mat4::Identity();
+	// model stuff
+	Vec3 mod_pos = { 0.0f,0.0f,2.0f };
 	float theta_x = 0.0f;
 	float theta_y = 0.0f;
 	float theta_z = 0.0f;
-	float lpos_x = 0.0f;
-	float lpos_y = 0.0f;
-	float lpos_z = 0.6f;
-	float phi = 0.0f;
+	// light stuff
+	Vec4 l_pos = { 0.0f,0.0f,0.6f,1.0f };
 };
